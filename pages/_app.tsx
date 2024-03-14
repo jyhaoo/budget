58ec6356caf8d72ebd5819e787f062cc9714f5ba
Jason Hao <jyhao1998@gmail.com>
2024-03-14 10:15:14 -0700

    feat: add supabase client and session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import SiteLayout from "@/components/SiteLayout";
 import { Nunito } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import {
+  SessionContextProvider,
+  Session,
+  SupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { Database } from "../type/schema";
+import type { AppProps } from "next/app";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -9,19 +17,32 @@ const nunito = Nunito({
   weight: ["400", "500", "600", "700", "800"],
 });
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   const [queryClient] = useState(function () {
     return new QueryClient();
   });
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <main className={nunito.className}>
-          <SiteLayout>
-            <Component {...pageProps} />
-          </SiteLayout>
-        </main>
-      </QueryClientProvider>
+      <SessionContextProvider
+        supabaseClient={
+          supabaseClient as SupabaseClient<Database, "public", any>
+        }
+        initialSession={pageProps.initialSession}
+      >
+        <QueryClientProvider client={queryClient}>
+          <main className={nunito.className}>
+            <SiteLayout>
+              <Component {...pageProps} />
+            </SiteLayout>
+          </main>
+        </QueryClientProvider>
+      </SessionContextProvider>
     </>
   );
 }
